dca66e63dcc8862ca78ac040670b6320b38e3f5c
Blanca Bigeriego <blancabcbt@gmail.com>
2024-03-13 19:37:40 +0100

    uninstall primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ButtonComponent } from "./components/button/button.component";
 import { TeamComponent } from "./components/team/team.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, TeamComponent, ButtonComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
