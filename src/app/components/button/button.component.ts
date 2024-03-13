1e7548cdabbffddf47b6e109ae5084c890d68f41
Blanca Bigeriego <blancabcbt@gmail.com>
2024-03-13 19:46:09 +0100

    add button input to button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  selector: "app-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
+  @Input() public config: number = 0;
 
+  ngOnInit(): void {
+    //this.config =
+  }
 }
