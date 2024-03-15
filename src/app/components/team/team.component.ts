ff9fde59dea43e5e45d756549ef35cf163d0db46
Blanca Bigeriego <blancabcbt@gmail.com>
2024-03-15 20:14:45 +0100

    add logic to team component to get the teams score

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -9,4 +9,32 @@ export class TeamComponent {
   @Input() teamName: number = 0;
   public numberOfButtonsAndPoints: number[] = [1, 2, 3];
   public teamScore: string = "000:000";
+
+  public scorePoints(index: number): void {
+    if (!index || (index !== 1 && index !== 2 && index !== 3)) return;
+    this.getScore(index);
+  }
+
+  private getScore(points: number): void {
+    const [firstPart, secondPart] = this.teamScore.split(":");
+
+    let newSecondPart = parseInt(secondPart) + points;
+    let valueOverHundred = 0;
+
+    if (newSecondPart > 999) {
+      valueOverHundred = Math.floor(newSecondPart / 1000);
+      newSecondPart = newSecondPart % 1000;
+    }
+
+    const formattedFirstPart = this.formatScore(
+      parseInt(firstPart) + valueOverHundred
+    );
+    const formattedSecondPart = this.formatScore(newSecondPart);
+
+    this.teamScore = `${formattedFirstPart}:${formattedSecondPart}`;
+  }
+
+  private formatScore(score: number): string {
+    return score.toString().padStart(3, "0");
+  }
 }
