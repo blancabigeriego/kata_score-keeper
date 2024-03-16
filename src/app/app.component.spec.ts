d69a1b07a0ecc58c45409e3fca268f8106daca5b
Blanca Bigeriego <blancabcbt@gmail.com>
2024-03-16 11:07:45 +0100

    test: add test to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
-import { TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 
 describe("AppComponent", () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -12,15 +14,26 @@ describe("AppComponent", () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it("should create the app", () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kata-score-keeper'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual("kata-score-keeper");
+  it("should initialize title, numberOfTeams, and teamColors correctly", () => {
+    expect(component.title).toBe("kata-score-keeper");
+    expect(component.numberOfTeams).toEqual([1, 2]);
+    expect(component.teamColors).toEqual(["red", "blue", "green"]);
+  });
+
+  it("should render app-team component for each team", () => {
+    const teamElements: NodeListOf<Element> =
+      fixture.nativeElement.querySelectorAll(".teams-container app-team");
+    expect(teamElements.length).toBe(component.numberOfTeams.length);
   });
 });
